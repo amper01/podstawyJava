5bed6e10be4b802915bd06ea10801a024f2b0e83
amper01 <allternauta@gmail.com>
2015-02-19 10:57:25 +0100

    Pytan parę

--- OdcinkiRozwiazanie.java
+++ OdcinkiRozwiazanie.java
@@ -7,19 +7,31 @@ import java.util.Arrays;
  */
 
 class Rozwiazanie {
+
     boolean czyPokryte(int wielkosc, int[] begin, int[] end) {
-        //przedzial rozpatrywany to 0...wielkosc-1; czyli np dla wielkosc=11: [0...10]
-        //odcinki zadane sa przez ich punkty koncowe
-//        int[] begin = {0, 4, 6, 3};
-//        int[] end = {1, 6, 7, 5};
-        // tzn. odcinek 0 pokrywa przedzial [begin[0]...end[0]], czyli punkty 0,1,
-        // odcinek 1 pokrywa przedial [begin[1]...end[1]] czyli punkty 4,5,6
-        //prosze sprawdzic, czy wszystkie punkty 0...10 (brzegi wlaczajac)
-        //sa pokryte przez przynajmniej jeden z odcinkow
+       
+       //Pytania 
+        /*ten element kodu - zmienna typu boolean nazwa "czyPokryte" (i właśnie co i jak się zamieszcza w nawiasie i dlaczego?)
+        rozumiem, że są to deklaracje zmiennej int i dwóch tablic intow. - ale dlaczego są zdeklarowane w nawiasie i jaka jest różnica 
+        między deklaracją (int wielkość, int[]begin, int[] end) a deklarację 
+        
+        boolean czyPokryte {
+            int wielkość;
+            int [] begin, int[]end
+            ??
+        //....
+      */
+      
+        
         int[] pokrycie = new int[wielkosc];
+        /*
+        gdzie została określona wartość zmiennej "wielkosc", bo deklaracja zmiennej jest w nawiasie. 
+        */
+        
         int nOdciknow = begin.length;
         for (int i = 0; i < nOdciknow; i++) {
-            for (int at = begin[i]; at <= end[i]; at++) {
+            for (int at = begin[i]; at <= end[i]; at++) 
+            {
                 pokrycie[at]++;
             }
             System.out.println(Arrays.toString(pokrycie));
@@ -28,7 +40,7 @@ class Rozwiazanie {
         for (int i = 0; i < wielkosc; i++) {
             if (pokrycie[i]==0) pokryty=false;
         }
-        return pokryty;    ///
+        return pokryty;    // Czy można prosić o więcej informacji na temat konstrukcji z returnem???
     }
 }
 
@@ -38,5 +50,9 @@ public class Odcinki {
 
         Rozwiazanie rozwiazanie = new Rozwiazanie();
         System.out.println(rozwiazanie.czyPokryte(11, new int[]{0,2,3},new int[]{2,3,10}));
+        /*a rozumiem, że tutaj deklarujemy wartości zmiennych zdefiniowanych w "metodzie"? 
+        Ale skoro tak to dlaczego trzeba na nowo definiowac tablice intów skoro również sa zdefiniowane w metodzie??
+        
+        */
     }
 }
